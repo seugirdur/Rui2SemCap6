a4d3ba923ec9f4e4ea2ae974ac6b845b01eff89c
Gabriel Rudrigues Lima <gabriel.lima137170@gmail.com>
2023-10-05 14:45:31 -0300

    fnializado e revisado

--- cap6.c
+++ cap6.c
@@ -1,4 +1,5 @@
 #define ex65
+
 #include <stdio.h>
 
 
@@ -68,7 +69,7 @@ main()
     result = num1;
     printf("A multiplicacao dos dois numeros e: %d * %d = %d\n", num1, num2, result *= num2);
     result = num1;
-    printf("A divisao dos dois numeros e: %d / %d = %f\n", num1, num2, (float)result / (float)num2);
+    printf("A divisao dos dois numeros e: %d / %d = %f\n", num1, num2, (float)result /= (float)num2);
     result = num1;
 
     printf("\nDigite X para sair  \n");
@@ -119,64 +120,29 @@ main()
     printf("digite o ano da data atual: \n");
     scanf("%d", &a);
 
-    if ( m > nacm )
-        resa = ( a - naca ) +1;
-    else
-        if ( m < nacm )
-            resa = ( a - naca );
-        else
-            if( d > nacd )
-                resa = ( a - naca ) + 1;
-            else
-                if( d < nacd )
-                    resa = ( a - naca );
-                else
-                    resa = ( a - naca ) + 1;
+
+    if (m > nacm || (m == nacm && d >= nacd)) {
+        resa = a - naca;
+    } else {
+        resa = a - naca - 1;
+    }
 
     printf(" Sua idade em anos e: %d anos\n", resa);
 
 
-    if ( m > nacm)
-        resm = ((( a - naca) + 1 ) * 12 ) + ( m - nacm);
-    else
-        if ( m < nacm)
-            resm = (( a - naca) * 12 ) + ( nacm - m);
-        else
-            if ( d > nacd)
-                resm = ((( a - naca) + 1 ) * 12 ) + ( m - nacm);
-            else
-                resm = (( a - naca) * 12 ) + ( nacm - m);
+    if (d >= nacd) {
+        resm = (resa * 12) + (m - nacm);
+    } else {
+        resm = (resa * 12) + (m - nacm - 1);
+    }
 
     printf(" Sua idade em meses e: %d meses \n", resm);
 
-
-    if( m > nacm && d > nacd )
-        resd = (((( a - naca) + 1 ) * 12 ) * 30 ) +  (((( m - nacm) + 1) * 12 ) * 30 ) + ( d - nacd);
-    else
-        if ( m > nacm && d == nacd )
-            resd = (((( a - naca) + 1 ) * 12 ) * 30 ) + (((( m - nacm) + 1) * 12 ) * 30 );
-        else
-            if ( m > nacm && d < nacd )
-                resd = (((( a - naca) + 1 ) * 12 ) * 30 ) +  ((( m - nacm) * 12 ) * 30 ) + ( nacd - d );
-    else
-        if( m < nacm && d > nacd )
-            resd = ((( a - naca)  * 12 ) * 30 ) + ((( nacm - m) * 12 ) * 30 ) + ( d - nacd);
-        else
-            if ( m < nacm && d == nacd )
-                resd = ((( a - naca)  * 12 ) * 30 ) + ((( nacm - m) * 12 ) * 30 ) + 1;
-            else
-                if ( m < nacm && d < nacd )
-                    resd = ((( a - naca)  * 12 ) * 30 ) + ((( nacm - m) * 12 ) * 30 ) + ( nacd - d);
-    else
-        if( m == nacm && d > nacd )
-            resd = (((( a - naca) + 1 ) * 12 ) * 30 ) +  (((( m - nacm) + 1) * 12 ) * 30 ) + ( d - nacd);
-        else
-            if( m == nacm && d == nacd )
-                resd = (((( a - naca) + 1 ) * 12 ) * 30 ) +  (((( m - nacm) + 1) * 12 ) * 30 ) ;
-            else
-                resd = ((( a - naca) * 12 ) * 30 ) +  ((( m - nacm) * 12 ) * 30 ) + ( nacd - d);
-
-
+    if (d >= nacd) {
+        resd = (resa * 365) + ((m - nacm) * 30) + (d - nacd);
+    } else {
+        resd = (resa * 365) + ((m - nacm - 1) * 30) + (30 - nacd + d);
+    }
     printf(" Sua idade em dias e: %d dias\n", resd);
 
     printf("\nDigite X para sair  \n");
@@ -311,32 +277,44 @@ main()
 
 #ifdef ex65
 
-int a, b;
-int i;
-
-main()
-{
+int main() {
     /* 5 - Escreva um programa que receba via teclado 2 numeros inteiros e execute
     as operacoes logicas bit a bit AND, OU, OU EXCLUSIVO e imprima-os no
     video no formato decimal e hexadecimal. */
 
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &a);
-    while( a / 2 > 1)
-    {
-        i++;
+    int num1, num2;
+    printf("Digite o primeiro número inteiro: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número inteiro: ");
+    scanf("%d", &num2);
+
+    printf("Operação AND:\n");
+    printf("Decimal: %d & %d = %d\n", num1, num2, num1 & num2);
+    printf("Hexadecimal: %X & %X = %X\n\n", num1, num2, num1 & num2);
+
+    printf("Operação OR:\n");
+    printf("Decimal: %d | %d = %d\n", num1, num2, num1 | num2);
+    printf("Hexadecimal: %X | %X = %X\n\n", num1, num2, num1 | num2);
+
+    printf("Operação XOR:\n");
+    printf("Decimal: %d ^ %d = %d\n", num1, num2, num1 ^ num2);
+    printf("Hexadecimal: %X ^ %X = %X\n\n", num1, num2, num1 ^ num2);
+
+
+    printf("\nDigite X para sair  \n");
+    char saida;
+    while (saida != 'X' && saida != 'x') {
+        scanf(" %c", &saida);
+
+        if (saida != 'X' && saida != 'x') {
+            printf("Erro, digite novamente  \n");
+        }
     }
 
-    printf("Digite o segundo numero: ");
-    scanf("%d", &b);
-
-
-
+    return 0;
 
 
 }
-
-
-
 
 #endif // ex65
